cde66f56e78dd417535ea5117304a7f24e33a811
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2018-04-28 11:01:01 +0200

    encoding of nominal features, support more na types, int check, remove ignored attributes, set correct arff target

--- project/experiments/test.py
+++ project/experiments/test.py
@@ -2,8 +2,21 @@
 import numpy as np
 from project.utils import DataLoader, introduce_missing_values
 
-data_loader = DataLoader("musk")
-features, labels, types = data_loader.load_data()
-features = introduce_missing_values(features, missing_rate=0.1)
+data_loader = DataLoader("test")
+features, labels, types = data_loader.load_data("csv")
+#features = introduce_missing_values(features, types, missing_rate=0.5)
 
+# np.unique(features["leaves"])
 features.head()
+
+
+# %%
+labels.head()
+
+
+# %%
+types.head()
+
+# %%
+features["f2"][0] = np.nan
+np.unique(features["f2"])
--- project/utils/data_modifier.py
+++ project/utils/data_modifier.py
@@ -1,19 +1,24 @@
+"""
+    Used to introduce missing values into artificial datasets.
+    Could be used for real-world-datasets as well, but final missing rate will
+    be different from specified one if missing values are already present.
+"""
 import numpy as np
 
 
-def introduce_missing_values(data, missing_rate=0.25, missing_type="MCAR"):
+def introduce_missing_values(data, feature_types, missing_rate=0.25, missing_type="MCAR"):
     # Introduce missing values by a specified method
     total_samples = data.shape[0] * data.shape[1]
     n_removals = round(missing_rate * total_samples)
 
     incomplete_data = data.copy()
     if missing_type == "MCAR":
-        return remove_with_mcar(incomplete_data, total_samples, n_removals)
+        return remove_with_mcar(incomplete_data, feature_types, total_samples, n_removals)
     else:
         raise NotImplementedError
 
 
-def remove_with_mcar(data, total_samples, n_removals):
+def remove_with_mcar(data, feature_types, total_samples, n_removals):
     # Introduce missing values using MCAR
     mask_indices = np.random.choice(total_samples, n_removals, replace=False)
     mask = np.zeros(total_samples, dtype=bool)
@@ -21,4 +26,7 @@ def remove_with_mcar(data, total_samples, n_removals):
     mask = np.reshape(mask, data.shape)
 
     data = data.where(mask == False)
+    for i in range(data.shape[1]):
+        if feature_types[i] == "nominal":
+            data.iloc[:, i].replace(np.nan, b"?", True)
     return data
