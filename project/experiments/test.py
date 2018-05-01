ccb26b05d052e8ab43c82a2dc73680f25afc4cfc
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2018-05-01 13:46:40 +0200

    added rknn with imputation method

--- project/experiments/test.py
+++ project/experiments/test.py
@@ -2,13 +2,23 @@
 import numpy as np
 import pandas as pd
 from project.utils.data_loader import DataLoader
-from project.utils.data_modifier import introduce_missing_values
 
 data_loader = DataLoader()
-features, labels, types = data_loader.load_data("iris", "csv")
-#features = introduce_missing_values(features, types, missing_rate=0.5)
+features, labels, types = data_loader.load_data("iris", "arff")
 
-# np.unique(features["leaves"])
+"""
 print(features.head())
 print(labels.head())
 print(types.head())
+"""
+
+# %%
+from project.utils.data_modifier import introduce_missing_values
+
+features = introduce_missing_values(features, types)
+features.head()
+
+# %%
+from project.randomKNN.random_knn import RKNN
+rknn = RKNN(features, labels, types)
+rknn.fit_transform()
