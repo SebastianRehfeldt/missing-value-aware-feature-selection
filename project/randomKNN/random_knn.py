1907a67f85ac40181231b3739bb25bd67ce662a6
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2018-05-07 14:25:31 +0200

    subspacing base class

--- project/randomKNN/random_knn.py
+++ project/randomKNN/random_knn.py
@@ -9,16 +9,17 @@ from fancyimpute import KNN as knn_imputer
 
 from project.randomKNN.knn import KNN as knn_classifier
 from project.shared.selector import Selector
+from project.shared.subspacing import Subspacing
 
 
-class RKNN(Selector):
+class RKNN(Selector, Subspacing):
 
     def __init__(self, data, **kwargs):
         super().__init__(data)
 
     def _init_parameters(self, parameters):
         self.params = {
-            "n_knn": parameters.get("n_knn", int(self.data.shape[1]**2 / 2)),
+            "n": parameters.get("n", int(self.data.shape[1]**2 / 2)),
             "m": parameters.get("m", int(np.sqrt(self.data.shape[1]))),
             "n_neighbors": parameters.get("n_neighbors", 3),
             "k": parameters.get("k", 3),
@@ -31,22 +32,7 @@ class RKNN(Selector):
         score_map = self._evaluate_subspaces(subspaces)
         return self._deduce_feature_importances(score_map)
 
-    def _get_unique_subscapes(self):
-        subspaces = [list(np.random.choice(self.data.features.columns, self.params["m"], replace=False))
-                     for i in range(self.params["n_knn"])]
-        subspaces.sort()
-        return list(subspaces for subspaces, _ in itertools.groupby(subspaces))
-
-    def _evaluate_subspaces(self, subspaces):
-        score_map = defaultdict(list)
-        for subspace in subspaces:
-            features = self.data.features[subspace]
-            mean_score = self._calculate_subspace_score(features)
-            for feature in features:
-                score_map[feature].append(mean_score)
-        return score_map
-
-    def _calculate_subspace_score(self, features):
+    def _evaluate_subspace(self, features):
         if self.params["method"] == "imputation" and features.isnull().values.any():
             features.update(knn_imputer(k=3, verbose=False).complete(features))
 
@@ -65,5 +51,10 @@ class RKNN(Selector):
         scores = cross_val_score(clf, features, y, cv=cv, scoring=scoring)
         return np.mean(scores)
 
-    def _deduce_feature_importances(self, score_map):
+    def _deduce_feature_importances(self, knowledgebase):
+        score_map = defaultdict(list)
+        for subspace in knowledgebase:
+            for feature in subspace["features"]:
+                score_map[feature].append(subspace["score"])
+
         return dict((k, np.mean(v)) for k, v in score_map.items())
