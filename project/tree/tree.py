4f8df2f4c43d28e511f4b6a5a80063f80cec3bca
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2018-05-13 18:25:18 +0200

    fixed nominal features for orange

--- project/tree/tree.py
+++ project/tree/tree.py
@@ -1,6 +1,7 @@
 """
     Tree Classifier class using Orange implementation
 """
+import pandas as pd
 from Orange.data import Table, Domain
 from Orange.classification import TreeLearner as TreeClassifier
 from Orange.regression import TreeLearner as TreeRegressor
@@ -10,16 +11,15 @@ from project.utils.assertions import assert_series, assert_data, assert_l_type, 
 
 class Tree():
 
-    def __init__(self, f_types, l_type, **kwargs):
+    def __init__(self, data, **kwargs):
         """
         Class which predicts label for unseen samples
 
         Arguments:
-            f_types {pd.series} -- Series of feature types
-            l_type {str} -- Type of label
+            data {Data} -- Data Object
         """
-        self.f_types = assert_series(f_types)
-        self.l_type = assert_l_type(l_type)
+        self.data = data
+        self.domain = data.to_table().domain
 
     def fit(self, X, y):
         """
@@ -29,14 +29,18 @@ class Tree():
             X {[df]} -- Dataframe containing the features
             y {pd.series} -- Label vector
         """
-        types = assert_types(self.f_types[X.columns.values], X.columns.values)
-        data = Data(X, y, types, self.l_type, X.shape).to_table()
-        if self.l_type == "nominal":
-            self.labels = data.domain.class_var.values
-            self.tree = TreeClassifier().fit_storage(data)
+
+        self.attributes = [
+            a for a in self.domain.attributes if a.name in X.columns.values]
+        s_domain = Domain(self.attributes, class_vars=self.domain.class_var)
+
+        rows = pd.concat([X, y], axis=1).values.tolist()
+        train = Table.from_list(domain=s_domain, rows=rows)
+
+        if self.data.l_type == "nominal":
+            self.tree = TreeClassifier().fit_storage(train)
         else:
-            self.domain = data.domain
-            self.tree = TreeRegressor().fit_storage(data)
+            self.tree = TreeRegressor().fit_storage(train)
         return self
 
     def predict(self, X):
@@ -47,13 +51,13 @@ class Tree():
             X {[df]} -- Dataframe containing the features
         """
         X = assert_df(X)
-        if self.l_type == "nominal":
-            predictions = self.tree(X.values)
-            predictions = [self.labels[pred] for pred in predictions]
-        else:
-            domain = Domain(list(self.domain.attributes))
-            test = Table.from_list(domain, X.values.tolist())
-            predictions = self.tree(test.X)
+        domain = Domain(list(self.attributes))
+        test = Table.from_list(domain, X.values.tolist())
+
+        predictions = self.tree(test.X)
+        if self.data.l_type == "nominal":
+            labels = self.domain.class_var.values
+            predictions = [labels[pred] for pred in predictions]
         return predictions
 
     def get_params(self, deep=False):
@@ -64,6 +68,5 @@ class Tree():
             deep {bool} -- Deep copy (default: {False})
         """
         return {
-            "f_types": self.f_types,
-            "l_type": self.l_type,
+            "data": self.data,
         }
