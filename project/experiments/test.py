78b00bb687d10ad934ed3b1feffa0aa8791bb747
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2018-05-17 08:44:44 +0200

    added yapf and flake

--- project/experiments/test.py
+++ project/experiments/test.py
@@ -8,7 +8,6 @@ data = data_loader.load_data("ionosphere", "arff")
 data = data_loader.load_data("boston", "arff")
 data = data_loader.load_data("iris", "arff")
 data = data_loader.load_data("credit-approval", "arff")
-
 """
 print(data.X.head())
 print(data.y.head())
@@ -16,20 +15,17 @@ print(data.f_types.head())
 print(data.l_type)
 """
 
-
 # %%
 from project.utils.data_modifier import introduce_missing_values
 
 data = introduce_missing_values(data, missing_rate=0.5)
 # data.X.head()
 
-
 # %%
 from project.utils.data_scaler import scale_data
 
 data = scale_data(data)
 # data.X.head()
-
 
 # %%
 from time import time
@@ -43,35 +39,24 @@ from project.mutual_info.mi_filter import MI_Filter
 
 knn = KNN(data.f_types, data.l_type)
 
-pipe1 = Pipeline(steps=[
-    ('reduce', RKNN(data)),
-    ('classify', knn)
-])
+pipe1 = Pipeline(steps=[('reduce', RKNN(data)), ('classify', knn)])
 
 pipe2 = Pipeline(steps=[
     ("imputer", Imputer(data, method="mice")),
     ('classify', knn),
 ])
 
-pipe3 = Pipeline(steps=[
-    ('classify', knn)
-])
+pipe3 = Pipeline(steps=[('classify', knn)])
 
-pipe4 = Pipeline(steps=[
-    ('reduce', MI_Filter(data)),
-    ('classify', knn)
-])
+pipe4 = Pipeline(steps=[('reduce', MI_Filter(data)), ('classify', knn)])
 
-pipe5 = Pipeline(steps=[
-    ('classify', Tree(data))
-])
+pipe5 = Pipeline(steps=[('classify', Tree(data))])
 
 pipe6 = Pipeline(steps=[
     ("imputer", Imputer(data, method="mice")),
     ('reduce', RKNN(data)),
     ('classify', knn),
 ])
-
 """
 X_new = RKNN(data).fit_transform()
 types = pd.Series(X_new.columns.values)
@@ -84,8 +69,7 @@ pipe7 = Pipeline(steps=[
 ])
 """
 
-
-#pipelines = [pipe1, pipe2, pipe3, pipe4, pipe5, pipe6, pipe7]
+# pipelines = [pipe1, pipe2, pipe3, pipe4, pipe5, pipe6, pipe7]
 pipelines = [pipe4]
 
 scores = []
@@ -95,14 +79,15 @@ scoring = "accuracy" if data.l_type == "nominal" else "neg_mean_squared_error"
 
 for pipe in pipelines:
     start = time()
-    scores.append(cross_val_score(pipe, data.X, data.y,
-                                  cv=cv, scoring=scoring, n_jobs=1))
+    scores.append(
+        cross_val_score(
+            pipe, data.X, data.y, cv=cv, scoring=scoring, n_jobs=1))
     times.append(time() - start)
 
 print("Results\n\n")
 for i, score in enumerate(scores):
     print("Pipe{:d} with mean {:.3f} took {:.3f}s.".format(
-        i+1, np.mean(score), times[i]))
+        i + 1, np.mean(score), times[i]))
     print("Detailed scores: ")
     print(score)
     print("\n")
