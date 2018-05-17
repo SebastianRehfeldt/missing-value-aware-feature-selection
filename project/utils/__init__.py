2d5c5e6650acdc91bc4043ec98ac3dc724aeee05
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2018-05-17 09:43:18 +0200

    improved tree, knn and imputer

---
+++ project/utils/__init__.py
@@ -0,0 +1,2 @@
+from .assertions import assert_data, assert_df, assert_l_type
+from .assertions import assert_series, assert_types
--- project/utils/data.py
+++ project/utils/data.py
@@ -1,13 +1,12 @@
 from copy import deepcopy
 import numpy as np
 import pandas as pd
-from project.utils.assertions import assert_data, assert_df, assert_types, assert_series
+from project.utils import assert_data, assert_df, assert_types, assert_series
 from Orange.data.variable import DiscreteVariable, ContinuousVariable
 from Orange.data import Domain, Table
 
 
 class Data():
-
     def __init__(self, X, y, f_types, l_type, shape):
         self.X = X
         self.y = y
@@ -17,25 +16,24 @@ class Data():
 
         assert_data(self)
 
-    def replace(self, **kwargs):
-        new_data = deepcopy(self)
+    def replace(self, copy=False, **kwargs):
+        new_data = deepcopy(self) if copy else self
 
         for k, v in kwargs.items():
             setattr(new_data, k, v)
 
         return assert_data(new_data)
 
-    def get(self, subspace):
-        # TODO check if works for 1d subspace
+    def get_subspace(self, subspace):
         new_X = assert_df(self.X[subspace])
         new_types = assert_types(self.f_types[subspace], subspace)
         return new_X, new_types
 
-    def select(self, subspace):
-        new_X, types = self.get(subspace)
-        return self.replace(X=new_X, f_types=types, shape=new_X.shape)
+    def select_data(self, subspace, copy=False):
+        new_X, types = self.get_subspace(subspace)
+        return self.replace(copy, X=new_X, f_types=types, shape=new_X.shape)
 
-    def inverse(self):
+    def inverse_data(self, copy=False):
         """
         Replaces feature matrix with label vector
         """
@@ -43,7 +41,7 @@ class Data():
         new_X.columns = [self.y.name]
 
         new_types = assert_types(self.l_type, self.y.name)
-        return self.replace(X=new_X, f_type=new_types)
+        return self.replace(copy, X=new_X, f_type=new_types)
 
     def select_inverse(self, col):
         """
@@ -65,22 +63,20 @@ class Data():
         return assert_data(inversed)
 
     def to_table(self):
-        f_array = []
-        for i, t in enumerate(self.f_types):
-            f_name = self.X.columns[i]
-            if t == "numeric":
-                f_array.append(ContinuousVariable(f_name))
-            else:
-                values = [v for v in np.unique(self.X[f_name])]
-                f_array.append(DiscreteVariable(f_name, values=values))
-
-        l_name = self.y.name
-        if self.l_type == "nominal":
-            values = [v for v in np.unique(self.y)]
-            class_var = DiscreteVariable(f_name, values=values)
-        else:
-            class_var = ContinuousVariable(l_name)
+        attributes = [
+            Data.get_variable(c_type, self.X.columns[i], self.X.iloc[:, i])
+            for i, c_type in enumerate(self.f_types)
+        ]
+        class_var = Data.get_variable(self.l_type, self.y.name, self.y)
 
         combined = pd.concat([self.X, self.y], axis=1)
-        domain = Domain(f_array, class_vars=class_var)
+        domain = Domain(attributes, class_vars=class_var)
         return Table.from_list(domain=domain, rows=combined.values.tolist())
+
+    @staticmethod
+    def get_variable(c_type, name, column):
+        if c_type == "nominal":
+            values = [v for v in np.unique(column)]
+            return DiscreteVariable(name, values=values)
+        else:
+            return ContinuousVariable(name)
