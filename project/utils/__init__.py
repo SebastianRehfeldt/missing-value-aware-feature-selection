8517259eafbda756622b176c569c5e589d0eec02
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2018-05-22 09:57:50 +0200

    added row deleter

--- project/utils/__init__.py
+++ project/utils/__init__.py
@@ -3,12 +3,14 @@ from .loading import DataLoader
 from .data_modifier import introduce_missing_values
 from .data_scaler import scale_data
 from .imputer import Imputer
+from .deleter import Deleter
 
 __all__ = [
     "DataLoader",
     "introduce_missing_values",
     "scale_data",
     "Imputer",
+    "Deleter",
     'Data',
     'assert_df',
     'assert_series',
