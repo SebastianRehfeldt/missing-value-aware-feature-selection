5ee43536f22ab2c98c0ff298d1096f23f8384e2c
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2018-05-24 11:32:04 +0200

    implement ranking and deduction for rar

--- project/rar/rar.py
+++ project/rar/rar.py
@@ -1,9 +1,14 @@
 """
     RaR class for feature selection
 """
+import random
 import numpy as np
+from collections import defaultdict
+
 from project.base import Subspacing
 from .optimizer import deduce_relevances
+from .contrast import calculate_contrast
+from .slicing import get_slices
 
 
 class RaR(Subspacing):
@@ -19,16 +24,6 @@ class RaR(Subspacing):
         self.params["subspace_size"] = kwargs.get("subspace_size",
                                                   (1, max_size))
 
-    def _complete(self, names, types, target):
-        if self.params.get("approach", "deletion") == "deletion":
-            indices = self.data.X[names + [target]].notnull().apply(
-                all, axis=1)
-            new_X = self.data.X[names][indices]
-            new_t = self.data.X[target][indices]
-            new_y = self.data.y[indices]
-
-        return new_X, new_y, new_t
-
     def _evaluate_subspace(self, X, types):
         """
         Evaluate a subspace using hics measure
@@ -37,9 +32,6 @@ class RaR(Subspacing):
             X {df} -- Dataframe containing the features
             types {pd.series} -- Series containing the feature types
         """
-        from .contrast import calculate_contrast
-        from .slicing import get_slices
-        import random
 
         names = X.columns.tolist()
         open_features = [
@@ -49,6 +41,7 @@ class RaR(Subspacing):
 
         # TODO: deletion with target removes more samples than neccessary
         # TODO: increase iterations when removing or imputing data
+        # TODO: implement imputation
         new_X, new_y, new_t = self._complete(names, types, target)
 
         relevances = []
@@ -79,11 +72,72 @@ class RaR(Subspacing):
         Arguments:
             knowledgebase {list} -- List of subspace results
         """
-        # TODO: deduce scores
-
-        from pprint import pprint
-        pprint(knowledgebase)
         relevances = deduce_relevances(self.data.X.columns.tolist(),
                                        knowledgebase)
-        pprint(relevances)
-        return {}
+        return self._calculate_ranking(knowledgebase, relevances)
+
+    def _complete(self, names, types, target):
+        if self.params.get("approach", "deletion") == "deletion":
+            indices = self.data.X[names + [target]].notnull().apply(
+                all, axis=1)
+            new_X = self.data.X[names][indices]
+            new_t = self.data.X[target][indices]
+            new_y = self.data.y[indices]
+
+        return new_X, new_y, new_t
+
+    def _combine_scores(self, rel, red):
+        return 2 * (1 - red) * rel / ((1 - red) + rel)
+
+    def _calculate_redundancy(self, samples, selected_features):
+        admissables = [
+            s for s in samples
+            if len(set(s[0]).intersection(selected_features)) > 0
+        ]
+
+        max_red = 0
+        for sample in admissables:
+            intersection = set(sample[0]).intersection(selected_features)
+
+            min_red = 1
+            for s in admissables:
+                if intersection.issubset(set(s[0])) and s[1] < min_red:
+                    min_red = s[1]
+
+            # a sample is justified if there exists no other admissable which
+            # contains the full intersection and has a lower redundancy score
+            if sample[1] <= min_red and sample[1] > max_red:
+                max_red = sample[1]
+
+        return max_red
+
+    def _calculate_ranking(self, knowledgebase, relevances):
+        redundancies = defaultdict(list)
+        for subset in knowledgebase:
+            features = subset["features"]
+            target = subset["score"]["target"]
+            redundancy = subset["score"]["redundancy"]
+            redundancies[target].append((features, redundancy))
+
+        ranking = {}
+        # add best feature to ranking with 0 redundancy
+        best = sorted(
+            relevances.items(), key=lambda k_v: k_v[1], reverse=True)[0]
+        ranking[best[0]] = self._combine_scores(best[1], 0)
+
+        open_features = self.data.X.columns.tolist()
+        open_features.remove(best[0])
+        # stepwise add features
+        while len(open_features) > 0:
+            best_score, best_feature = 0, None
+            for feature in open_features:
+                redundancy = self._calculate_redundancy(
+                    redundancies[feature], set(ranking.keys()))
+                score = self._combine_scores(relevances[feature], redundancy)
+                if score > best_score:
+                    best_score, best_feature = score, feature
+
+            ranking[best_feature] = best_score
+            open_features.remove(best_feature)
+
+        return ranking
