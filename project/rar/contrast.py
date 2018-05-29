810d60d64905d2604ab6e3d21e476ef41adde76b
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2018-05-29 10:16:22 +0200

    speed improvement ks test

--- project/rar/contrast.py
+++ project/rar/contrast.py
@@ -1,22 +1,23 @@
 import numpy as np
+from time import time
 
 
-def calculate_contrast(y_cond, y_type, cache):
+def calculate_contrasts(y, y_type, slices, cache):
     # TODO: improve speed by batch calculation of kld?
-    # TODO: check if kld or ks are > 1 (but no normalization for now)
-    # TODO: different value ranges from tests?
-    # use 1-exp(-KLD(P,Q)) to normalize kld
+
     return {
-        "numeric": _calculate_contrast_ks,
-        "nominal": _calculate_contrast_kld
-    }[y_type](y_cond, cache)
+        "numeric": _calculate_contrasts_ks,
+        "nominal": _calculate_contrasts_kld
+    }[y_type](y, slices, cache)
 
 
-def _calculate_contrast_ks(y_cond, cache):
+def _calculate_contrasts_ks(y, slices, cache):
+    sorted_y = cache["sorted"]
+    return [_calculate_contrast_ks(sorted_y, sorted_y[s]) for s in slices]
+
+
+def _calculate_contrast_ks(y, y_cond):
     # As in implementation from scipy but without sorting first data
-    # TODO: check how different dimensions are handled
-    y = cache["sorted"]
-    y_cond = np.sort(y_cond)
     n1 = y.shape[0]
     n2 = y_cond.shape[0]
     data_all = np.concatenate([y, y_cond])
@@ -25,14 +26,24 @@ def _calculate_contrast_ks(y_cond, cache):
     return np.max(np.absolute(cdf1 - cdf2))
 
 
-def _calculate_contrast_kld(y_cond, cache):
+def _calculate_contrasts_kld(y, slices, cache):
     values_m = cache["values"]
     probs_m = cache["probs"]
-    probs_c = {value: 1e-8 for value in values_m}
+    template = {value: 1e-8 for value in values_m}
+
+    start = time()
+    cdfs = np.zeros((len(slices), len(values_m)))
+    for i, s in enumerate(slices):
+        cdfs[i, :] = _calculate_contrast_kld(y[s], template)
+    print("CDF creation", time() - start)
+    return np.sum(cdfs * np.log2(cdfs / probs_m), axis=1)
+
+
+def _calculate_contrast_kld(y_cond, template):
+    probs_c = template.copy()
 
     values_c, counts_c = np.unique(y_cond, return_counts=True)
     value_dict = dict(zip(values_c, counts_c / len(y_cond)))
 
     probs_c.update(value_dict)
-    probs_c = list(probs_c.values())
-    return np.sum(probs_c * np.log2(probs_c / probs_m))
+    return list(probs_c.values())
