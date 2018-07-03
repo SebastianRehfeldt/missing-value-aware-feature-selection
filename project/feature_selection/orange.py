d9c1c5ec422c1829e7c69aa6a521e12dfc38a20c
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2018-07-03 17:45:49 +0200

    added fcbf, reliefF and rf feature ranking from Orange

---
+++ project/feature_selection/orange.py
@@ -0,0 +1,27 @@
+from project.base import Selector
+from Orange.preprocess.score import ReliefF, FCBF
+from Orange.classification import RandomForestLearner
+
+
+class Orange(Selector):
+    def _init_parameters(self, **kwargs):
+        """
+        Initialize params
+
+        Arguments:
+            parameters {dict} -- Parameter dict
+        """
+        super()._init_parameters(**kwargs)
+        self.params["eval_method"] = kwargs.get("eval_method", "relief")
+
+    def _fit(self):
+        table = self.data.to_table()
+        if self.params["eval_method"] == "relief":
+            scores = ReliefF(table, n_iterations=100)
+        elif self.params["eval_method"] == "fcbf":
+            scores = FCBF(table)
+        else:
+            scores = RandomForestLearner().score_data(table)[0]
+
+        for attr, score in zip(table.domain.attributes, scores):
+            self.feature_importances[attr.name] = score
