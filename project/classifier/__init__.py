6741cf97311694851d1af0d91871737ff7e5300e
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2018-08-03 15:54:06 +0200

    generic sklearn classifier class, closes #11

--- project/classifier/__init__.py
+++ project/classifier/__init__.py
@@ -1,7 +1,9 @@
 from .knn import KNN
 from .tree import Tree
+from .sklearn_classifier import SKClassifier
 
 __all__ = [
     'KNN',
     'Tree',
+    'SKClassifier',
 ]
--- project/classifier/sklearn_classifier.py
+++ project/classifier/sklearn_classifier.py
@@ -1,19 +1,28 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-from sklearn.linear_model import LogisticRegression as skl_logreg
+
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 
-class LogReg():
-    def __init__(self, f_types, **kwargs):
+class SKClassifier():
+    def __init__(self, f_types, kind="svm", **kwargs):
         self.f_types = f_types
+        self.kind = kind
         self.params = {}
+
+        self.fill_value = 42 if kind == "tree" else 0
 
     def encode(self, X):
         for col in X:
             if self.f_types[col] == "nominal":
-                # encode
+                nans = X[col] == "?"
                 X[col] = LabelEncoder().fit_transform(X[col])
+                X[col][nans] = np.nan
         return X
 
     def add_dummies(self, X):
@@ -26,17 +35,26 @@ class LogReg():
 
     def fit(self, X, y):
         X = self.encode(X)
-        X = self.add_dummies(X)
-        self.clf = skl_logreg(C=1e5).fit(X, y)
+        X = X.fillna(self.fill_value)
+
+        self.clf = {
+            "svm": SVC,
+            "bayes": GaussianNB,
+            "knn": KNeighborsClassifier,
+            "logreg": LogisticRegression,
+            "tree": DecisionTreeClassifier,
+        }[self.kind]()
+        self.clf.fit(X, y)
         return self
 
     def predict(self, X_test):
         X_test = self.encode(X_test)
-        X_test = self.add_dummies(X_test)
+        X_test = X_test.fillna(self.fill_value)
         return self.clf.predict(X_test)
 
     def get_params(self, deep=False):
         return {
             "f_types": self.f_types,
+            "kind": self.kind,
             **self.params,
         }
