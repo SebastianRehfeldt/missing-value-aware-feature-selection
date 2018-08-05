e4e679acd473bba2e0258a449f39554591bb434f
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2018-08-05 15:46:29 +0200

    finished visualization, closes #31

--- scripts/dataset_statistics.py
+++ scripts/dataset_statistics.py
@@ -1,94 +1,59 @@
 # %%
 import os
-import json
-import requests
-import numpy as np
-import pandas as pd
-
 from project import DATA_PATH
+from scripts.utils import *
 
 FOLDER = os.path.join(DATA_PATH, "statistics")
-
-try:
-    os.makedirs(FOLDER)
-    base = 'https://www.openml.org/api/v1/json/data/list/'
-    raw_data = json.loads(requests.get(base).content)
-    with open(os.path.join(FOLDER, "raw_data.json"), 'w') as outfile:
-        json.dump(raw_data, outfile, indent=4)
-
-except:
-    with open(os.path.join(FOLDER, "raw_data.json")) as infile:
-        raw_data = json.load(infile)["data"]["dataset"]
-        for d in raw_data:
-            metrics = d["quality"]
-            new_metrics = {x["name"]: x["value"] for x in metrics}
-            d["quality"] = new_metrics
+PLOT_FOLDER = os.path.join(FOLDER, "plots")
+os.makedirs(PLOT_FOLDER, exist_ok=True)
+raw_data = get_raw_data(FOLDER)
 
 # %%
-stats = {}
-stats["n_datasets"] = len(raw_data)
+print_dataset(raw_data, ["cleveland"])
 
 # %%
-raw_data
-
-# DONE
-# SIZE (instances, features)
-# MR BY DIMENSIONS
-
-# MIXED, NUMERICAL, NOMINAL FEATURES (PIE)
-# OVERVIEW TABLE FOR SPECIFIC DATASETS (instances, features, classes, majority/n, mr)
-# MAX NOMINAL ATTR DISTINCT VALUES (HIST)
-# MISSING RATES (HIST)
-# INSTANCES WITH MISSING VALUES (HIST, COMBINED WITH MRS AS SCATTER)
-# DATASETS WITH MISSING VALUES (PIE)
-# QUERY FOR FINDING DATASETS (size, mr, type)
-
+find(
+    raw_data,
+    f=(13, 13),
+    s=(303, 303),
+    c=(2, 2),
+    mr=(0.1, 3),
+    types=["mixed"],
+)
 
 # %%
-def plot_mr_by_dimensions(data, use_log=True, max_f=1e2, max_n=1e4, bin=True):
-    n = len(data)
-    dimensions = [None] * n
-    missing_rates = np.zeros(n)
-
-    show_in_plot = np.zeros(n, dtype=bool)
-    if use_log:
-        show_in_plot = np.ones(n, dtype=bool)
-
-    for i, d in enumerate(data):
-        metrics = d["quality"]
-        dimensions[i] = {
-            "samples": float(metrics["NumberOfInstances"]),
-            "features": float(metrics["NumberOfFeatures"]),
-        }
-
-        n_samples, n_features = list(dimensions[i].values())
-        if not use_log and n_features < max_f and n_samples < max_n:
-            show_in_plot[i] = True
-
-        n_values = n_samples * n_features
-        mr = float(metrics["NumberOfMissingValues"]) / n_values
-        missing_rates[i] = mr
-
-    colors = missing_rates[show_in_plot]
-    if bin:
-        colors = colors > 0
-
-    sorted_indices = np.argsort(colors)
-    colors = colors[sorted_indices]
-    dimensions = pd.DataFrame(dimensions)
-
-    df = dimensions[show_in_plot].iloc[sorted_indices]
-    ax = df.plot.scatter("samples", "features", c=colors, cmap='coolwarm')
-
-    if use_log:
-        ax.set_yscale('log')
-        ax.set_xscale('log')
-
-    return ax, (dimensions, missing_rates)
-
-
-fig, data = plot_mr_by_dimensions(raw_data, False, bin=False)
+# PLOT MR BY DIMENSIONS
+fig, data = plot_mr_by_dimensions(raw_data, False, bin_=False)
+fig.savefig(os.path.join(PLOT_FOLDER, "mr_by_dimensions.png"))
 dimensions, missing_rates = data
 
 # %%
-colors
+# GLOBAL STATISTICS TABLE
+summary, types = get_global_statistics(raw_data, missing_rates)
+summary.to_csv(os.path.join(PLOT_FOLDER, "summary_global.csv"))
+summary
+
+# %%
+# TABLES AND HIST FOR INCOMPLETE DATASETS
+mr = 0
+name = "summary_incomplete_{:.2f}.csv".format(mr)
+summary_mr, ratios = get_statistics_for_mr(raw_data, missing_rates, mr)
+summary_mr.to_csv(os.path.join(PLOT_FOLDER, name))
+
+fig = plot_ratios(ratios)
+name = "ratios_of_incomplete_instances_{:.2f}.png".format(mr)
+fig.savefig(os.path.join(PLOT_FOLDER, name))
+summary_mr
+
+# %%
+# MR DISTRIBUTION BY TYPE + PERCENTAGE OF INCOMPLETE DATASETS
+fig, summary_types = plot_by_type(missing_rates, types)
+fig.savefig(os.path.join(PLOT_FOLDER, "mr_by_type.png"))
+summary_types.to_csv(os.path.join(PLOT_FOLDER, "summary_types.csv"))
+summary_types
+
+# %%
+names = ["ionosphere", "heart-c", "semeion", "anneal"]
+overview = get_overview(raw_data, types, missing_rates, names)
+overview.to_csv(os.path.join(PLOT_FOLDER, "summary_datasets.csv"))
+overview
--- scripts/visualize_features.py
+++ scripts/visualize_features.py
@@ -1,9 +1,14 @@
 # %%
+import os
+from project import DATA_PATH
 from project.utils.plots import *
 from project.utils import DataLoader
 from project.utils import introduce_missing_values, scale_data
 
 name = "ionosphere"
+FOLDER = os.path.join(DATA_PATH, "plots", name)
+os.makedirs(FOLDER, exist_ok=True)
+
 data_loader = DataLoader(ignored_attributes=["molecule_name"])
 data = data_loader.load_data(name, "arff")
 print(data.shape, flush=True)
@@ -13,23 +18,28 @@ data = introduce_missing_values(data, missing_rate=mr)
 data = scale_data(data)
 
 # %%
-plot_nans_by_class(data)
+ax = plot_nans_by_class(data)
+ax.get_figure().savefig(os.path.join(FOLDER, "nans_by_class.png"))
 
 # %%
-plot_nan_percentage(data)
+ax = plot_nan_percentage(data)
+ax.get_figure().savefig(os.path.join(FOLDER, "nan_percentage.png"))
 
 # %%
-plot_nan_correlation(data)
+ax = plot_nan_correlation(data)
+ax.get_figure().savefig(os.path.join(FOLDER, "nan_correlation.png"))
 
 # %%
-plot_nan_dendogram(data)
+ax = plot_nan_dendogram(data)
+ax.get_figure().savefig(os.path.join(FOLDER, "nan_dendogram.png"))
 
 # %%
 features = ["a05", "a06"]
 show_boxplots(data, features)
 
 # %%
-show_scatter_plots(data, features)
+ax = show_scatter_plots(data, features)
+ax.get_figure().savefig(os.path.join(FOLDER, "scatter.png"))
 
 # %%
 show_correlation(data, features)
