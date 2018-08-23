326f04af63a5005adacc7b3abeaa0ca042540c72
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2018-08-23 11:43:29 +0200

    small fixes

--- experiments/synthethic/config_dataset.py
+++ experiments/synthethic/config_dataset.py
@@ -1,11 +1,11 @@
 DATASET_CONFIG = {
     "n_samples": 500,
-    "n_features": 40,
-    "n_independent": 40,
+    "n_features": 60,
+    "n_independent": 60,
     "n_dependent": 0,
     "n_relevant": 0,
     "n_discrete": 0,
-    "n_clusters": 10,
+    "n_clusters": 20,
     "y_flip": 0.01,
     "max_features_in_cluster": 2,
     "max_discrete_values": 10
