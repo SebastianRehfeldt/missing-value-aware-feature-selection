fd0c750617147d8f9d9c52038249cdf9f4839e1b
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2020-03-15 16:50:57 +0100

    introduce poetry and bump requirements

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,8 @@
 import os
 
-PROJECT_PATH = os.path.abspath(
-    os.path.join(os.path.abspath(__file__), os.pardir))
+__version__ = "0.1.0"
+
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir))
 ROOT_PATH = os.path.abspath(os.path.join(PROJECT_PATH, os.pardir))
 DATA_PATH = os.path.join(ROOT_PATH, "data")
 
